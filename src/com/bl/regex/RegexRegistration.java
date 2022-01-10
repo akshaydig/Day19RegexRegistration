480a2e325bb34bedf63e55115a6372c9b4ce654e
akshaydig <you@example.com>
2022-01-10 22:05:46 +0530

    Added: Check User Mobile Number

--- src/com/bl/regex/RegexRegistration.java
+++ src/com/bl/regex/RegexRegistration.java
@@ -6,23 +6,30 @@ import java.util.regex.Pattern;
 public class RegexRegistration {
 	String userFirstName = "^[A-Z][a-zA-Z]{3,}";
 	String userLastName = "^[A-Z][a-zA-Z]{3,}";
-	String userEmail = "^[A-Za-z0-9_-]+([A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)$";
-	
+	String userEmail = "^[A-Za-z0-9_-]+([.][A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)$";
+	String userMobileNumber = "^[0-9]{2}(\\s){1}[0-9]{10}$";
+
 	public void validateFirstName(String firstName) {
 		Pattern pattern = Pattern.compile(userFirstName);
 		Matcher matcher = pattern.matcher(firstName);
 		System.out.println(matcher.matches());
 	}
-	
+
 	public void validateLastName(String lastName) {
 		Pattern pattern = Pattern.compile(userLastName);
 		Matcher matcher = pattern.matcher(lastName);
 		System.out.println(matcher.matches());
 	}
-	
+
 	public void validateEmail(String email) {
 		Pattern pattern = Pattern.compile(userEmail);
 		Matcher matcher = pattern.matcher(email);
+		System.out.println(matcher.matches());
+	}
+
+	public void validateMobileNumber(String mobileNumber) {
+		Pattern pattern = Pattern.compile(userMobileNumber);
+		Matcher matcher = pattern.matcher(mobileNumber);
 		System.out.println(matcher.matches());
 	}
 }
--- src/com/bl/regex/TestClass.java
+++ src/com/bl/regex/TestClass.java
@@ -6,11 +6,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		RegexRegistration regex = new RegexRegistration();
 		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Enter Your Email: ");
-		String email = scan.nextLine();
-		
-		regex.validateEmail(email);
-		
+
+		System.out.print("Enter Your Mobile Number: ");
+		String mobileNumber = scan.nextLine();
+
+		regex.validateMobileNumber(mobileNumber);
+
 	}
 }
