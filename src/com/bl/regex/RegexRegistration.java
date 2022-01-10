11879d51a2d5b9764415a8bb9b2fb8cc0b3bbb6f
akshaydig <you@example.com>
2022-01-10 22:31:18 +0530

    Added: Check User At Least One Numeric Number in password

--- src/com/bl/regex/RegexRegistration.java
+++ src/com/bl/regex/RegexRegistration.java
@@ -8,7 +8,7 @@ public class RegexRegistration {
 	String userLastName = "^[A-Z][a-zA-Z]{3,}";
 	String userEmail = "^[A-Za-z0-9_-]+([.][A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)$";
 	String userMobileNumber = "^[0-9]{2}[\\s][0-9]{10}";
-	String userPassword = "^(?=.*[A-Z])(?=.*[0-9])([A-Z0-9a-z]){8,}";
+	String userPassword = "^(?=.*[0-9])(?=.*[!@#$%&^*)_(+}{?>:<]){1}+(?=.*[A-Z])([0-9A-Za-z]).{8,}$";
 
 	public void validateFirstName(String firstName) {
 		Pattern pattern = Pattern.compile(userFirstName);
