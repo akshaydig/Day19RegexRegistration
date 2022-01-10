05c6aa1ae4d586e72c75b900be52f19a8665d267
akshaydig <you@example.com>
2022-01-10 23:14:17 +0530

    Added: Check User Details Using Junit Parameterized

--- src/com/bl/test/TestClass.java
+++ src/com/bl/test/TestClass.java
@@ -2,56 +2,88 @@ package com.bl.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.bl.regex.RegexRegistration;
 
+@RunWith(Parameterized.class)
 public class TestClass {
+
+	String input;
+	boolean expectedResult;
+	RegexRegistration validation;
+
+	public TestClass(String input, boolean expectedResult) {
+		super();
+		this.input = input;
+		this.expectedResult = expectedResult;
+	}
+
 	@Test
 	public void testFirstNameHappy() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validateFirstName("Suhas");
 		assertEquals(true, output);
 	}
 
 	@Test
 	public void testFirstNameSad() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validateFirstName("suhas");
 		assertEquals(false, output);
 	}
 
 	@Test
 	public void testLastNameHappy() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validateLastName("Bhor");
 		assertEquals(true, output);
 	}
 
 	@Test
 	public void testLastNameSad() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validateLastName("bhor");
 		assertEquals(false, output);
 	}
 
-	@Test
-	public void testEmailHappy() {
-		RegexRegistration validation = new RegexRegistration();
-		boolean output = validation.validateEmail("abc.xyz@bl.co.in");
-		assertEquals(true, output);
+	@Parameters(name = "inputs()")
+	public static Collection inputs() {
+		return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc-100@yahoo.com", true },
+				{ "abc.100@yahoo.com", true }, { "abc111@abc.com", true }, { "abc-100@abc.net", true },
+				{ "abc.100@abc.com.au", true }, { "abc@1.com", true }, { "abc@gmail.com.com", true },
+				{ "abc+100@gmail.com.com", true }, { "abc+100@gmail.com", true }, { "abc@.com.com", false },
+				{ "abc123@gmail.a", false }, { "abc123@.com", false }, { "abc123@.com.com", false },
+				{ ".abc@abc.com", false }, { "abc()*@gmail.com", false }, { "abc@%*.com", false },
+				{ "abc..2002@gmail.com", false }, { "abc.@gmail.com", false }, { "abc@abc@gmail.com", false },
+				{ "abc@gmail.com.1a", false }, { "abc@gmail.com.aa.au^$", false } });
 	}
 
 	@Test
-	public void testEmailSad() {
-		RegexRegistration validation = new RegexRegistration();
-		boolean output = validation.validateEmail("abc.xyz@bl.co");
-		assertEquals(false, output);
+	public void testEmail() {
+		validation = new RegexRegistration();
+		boolean output = validation.validateEmail(input);
+		assertEquals(expectedResult, output);
 	}
+	/*
+	 * @Test public void testEmailHappy() { validation = new RegexRegistration();
+	 * boolean output = validation.validateEmail("abc.xyz@bl.co.in");
+	 * assertEquals(true, output); }
+	 * 
+	 * @Test public void testEmailSad() { validation = new RegexRegistration();
+	 * boolean output = validation.validateEmail("abc.xyz@bl.co");
+	 * assertEquals(false, output); }
+	 */
 
 	@Test
 	public void testMobileNumberHappy() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validateMobileNumber("91 1234567890");
 		assertEquals(true, output);
 	}
@@ -65,14 +97,14 @@ public class TestClass {
 
 	@Test
 	public void testPasswordHappy() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validatePassword("Abcde@123");
 		assertEquals(true, output);
 	}
 
 	@Test
 	public void testPasswordSad() {
-		RegexRegistration validation = new RegexRegistration();
+		validation = new RegexRegistration();
 		boolean output = validation.validatePassword("Abc@123");
 		assertEquals(false, output);
 	}
