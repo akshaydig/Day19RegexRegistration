1cfd5966f0e59486cd4396cfc5ec562c90044422
akshaydig <you@example.com>
2022-01-10 21:43:32 +0530

    Added: Check User Last Name

--- src/com/bl/regex/RegexRegistration.java
+++ src/com/bl/regex/RegexRegistration.java
@@ -5,10 +5,17 @@ import java.util.regex.Pattern;
 
 public class RegexRegistration {
 	String userFirstName = "^[A-Z][a-zA-Z]{3,}";
-	
+	String userLastName = "^[A-Z][a-zA-Z]{3,}";
+
 	public void validateFirstName(String firstName) {
 		Pattern pattern = Pattern.compile(userFirstName);
 		Matcher matcher = pattern.matcher(firstName);
+		System.out.println(matcher.matches());
+	}
+
+	public void validateLastName(String lastName) {
+		Pattern pattern = Pattern.compile(userLastName);
+		Matcher matcher = pattern.matcher(lastName);
 		System.out.println(matcher.matches());
 	}
 }
--- src/com/bl/regex/TestClass.java
+++ src/com/bl/regex/TestClass.java
@@ -6,11 +6,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		RegexRegistration regex = new RegexRegistration();
 		Scanner scan = new Scanner(System.in);
-
-		System.out.print("Enter Your Name: ");
-		String firstName = scan.nextLine();
-
-		regex.validateFirstName(firstName);
-
+		
+		System.out.print("Enter Your Last Name: ");
+		String lastName = scan.nextLine();
+		
+		regex.validateFirstName(lastName);
+		
 	}
 }
